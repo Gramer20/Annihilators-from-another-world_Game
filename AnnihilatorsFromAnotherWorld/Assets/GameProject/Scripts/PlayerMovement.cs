0537471389b6b5d8204b7cfe853804b4365f45a4
Gramer20 <alexeiduninovo@gmail.com>
2026-03-04 10:30:18 +0400

    Correct moving

--- AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/PlayerMovement.cs
+++ AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/PlayerMovement.cs
@@ -9,23 +9,63 @@ public class PlayerMovement : MonoBehaviour
 
     [SerializeField] private Transform _leftEnginePosition;
 
+    [SerializeField] private Rigidbody _rigidBody;
+
+    [SerializeField] private float _thrustEngine = 6f;
+
+    [SerializeField] private float _rotaryThrust = 5f;
+
+    [SerializeField] private float _maxSpeed = 10f;
+
     private Vector2 _direction;
 
-    public void OnMove(InputAction.CallbackContext context)
+    private void Update()
     {
-        _direction = context.ReadValue<Vector2>();
+        ApplyingForce(_thrustEngine, _leftEnginePosition);
+        ApplyingForce(_thrustEngine, _rightEnginePosition);
     }
 
     private void FixedUpdate()
     {
-        if(_direction != Vector2.zero)
+        if (_direction != Vector2.zero)
         {
+            if (_direction.x > 0)
+            {
+                ApplyingForce(CalculateRotaryForce(_rotaryThrust), _leftEnginePosition);
+            }
+            if (_direction.x < 0)
+            {
+                ApplyingForce(CalculateRotaryForce(_rotaryThrust * -1), _rightEnginePosition);
+            }
             
         }
     }
-
-    private void ApplyingForce(Transform enginePosition)
+    public void OnMove(InputAction.CallbackContext context)
     {
-        
+        if (context.performed)
+        {
+            _direction = context.ReadValue<Vector2>();
+        }
+        else if (context.canceled)
+        {
+            _direction = Vector2.zero;
+        }
+    }
+
+    private float CalculateRotaryForce(float thrust)
+    {
+        float correctThrust = _direction.x * thrust;
+
+        //correctThrust = Mathf.Clamp(correctThrust, 0, _maxSpeed);
+
+        return correctThrust;
+    }
+
+    private void ApplyingForce(float thrust, Transform enginePosition)
+    {
+        _rigidBody.AddForceAtPosition(
+            transform.forward * thrust,
+            enginePosition.position,
+            ForceMode.Acceleration);
     }
 }
