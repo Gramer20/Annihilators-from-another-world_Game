6cc7cec3aa912890dd8d2f18ea9b2dd6018e7460
Gramer20 <alexeiduninovo@gmail.com>
2026-03-12 23:15:55 +0400

    Add enemy in spawnArea

--- AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/SpawnEnemy.cs
+++ AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/SpawnEnemy.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnEnemy : MonoBehaviour
 {
@@ -8,23 +10,52 @@ public class SpawnEnemy : MonoBehaviour
 
     [SerializeField] private GameObject _enemyPrefab;
 
-    private float _spawnInterval = 1f;
+    private float _spawnInterval = 0.1f;
+    private int _spawnCount = 0;
 
     private void Start()
     {
         StartCoroutine(SpawnEnemies());
-
-        SphereCollider collider = _enemyPrefab.GetComponent<SphereCollider>();
-
-        float spawnRadius = collider.radius;
     }
 
     private IEnumerator SpawnEnemies()
     {
         WaitForSeconds waitForSeconds = new WaitForSeconds(_spawnInterval);
 
-        Instantiate(_enemyPrefab);
+        bool cicle = true;
 
-        yield return null;
+        while (cicle)
+        {
+            if(_spawnCount <= 20)
+            {
+                Instantiate(_enemyPrefab, RandomSpawnPosition(), _spawnArea.transform.rotation);
+                _spawnCount++;
+
+                yield return waitForSeconds;
+            }
+            else
+            {
+                cicle = false;
+            }
+        }
+    }
+
+    private Vector3 RandomSpawnPosition()
+    {
+        Vector3 position = _spawnArea.transform.position;
+
+        SphereCollider collider = _spawnArea.GetComponent<SphereCollider>();
+
+        float radius = collider.radius;
+
+        float randomPositionX = Random.Range(position.x - radius, position.x + radius);
+
+        float rangeZ = Mathf.Sqrt(Mathf.Pow(radius, 2) - Mathf.Pow((randomPositionX - position.x), 2));
+
+        float randomPositionZ = Random.Range(position.z - rangeZ, position.z + rangeZ);
+
+        Vector3 returnPosition = new Vector3(randomPositionX, 0, randomPositionZ);
+
+        return returnPosition;
     }
 }
