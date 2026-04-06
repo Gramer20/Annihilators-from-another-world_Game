178b1559affa943725d4ad668caa1601f9c3f91f
Gramer20 <alexeiduninovo@gmail.com>
2026-04-06 11:26:32 +0400

    Timer

--- AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/Timer.cs
+++ AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/Timer.cs
@@ -1,16 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Timers;
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] private TimerView _timerView;
-    
+    [SerializeField] private TMP_Text _timerView;
+
+    int _minute = 0;
+    int _second = 0;
+
     void Update()
     {
-        
+        _second = (_second + 1) * Convert.ToInt32(Time.deltaTime);
 
-        
+
     }
 }
--- AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/TimerView.cs
+++ AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/TimerView.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TimerView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private TMP_Text _timerVeiw;
 
-    // Update is called once per frame
-    void Update()
+    public void Display()
     {
-        
+        _timerVeiw.text = "{0:00}";
     }
 }
