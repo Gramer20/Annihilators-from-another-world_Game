47e06cd157ec91a5a05cccae49862331654011af
Gramer20 <alexeiduninovo@gmail.com>
2026-04-27 16:21:38 +0400

    Normalized spawn enemy

--- AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/SpawnEnemy.cs
+++ AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/SpawnEnemy.cs
@@ -6,10 +6,14 @@ public class SpawnEnemy : MonoBehaviour
 {
     [SerializeField] private Transform _spawnArea;
     [SerializeField] private GameObject _enemyPrefab;
-    [SerializeField] private int _maxEnemyCount = 20;
+    [SerializeField] private int _maxEnemyCount = 3;
 
-    private float waitTime = 1f;
-    private int _enemyCount = 0;
+    private List<GameObject> EnemyCountList = new List<GameObject>();
+
+    private WaitForSeconds wait = new WaitForSeconds(1f);
+    private WaitForSeconds wait_2 = new WaitForSeconds(3f);
+
+    private bool IsSpawning = true;
 
 
     private void Start()
@@ -17,20 +21,37 @@ public class SpawnEnemy : MonoBehaviour
         StartCoroutine(Spawn());
     }
 
+    private void FixedUpdate()
+    {
+        RemoveEnemy(); 
+    }
+
     private IEnumerator Spawn()
     {
-        WaitForSeconds wait = new WaitForSeconds(waitTime);
-
-        while (_enemyCount < _maxEnemyCount)
+        while (IsSpawning)
         {
-            GameObject enemy = Instantiate(_enemyPrefab, _spawnArea.position, RandomEnemyRotation());
+            while (EnemyCountList.Count < _maxEnemyCount)
+            {
+                GameObject enemy = Instantiate(_enemyPrefab, _spawnArea.position, RandomEnemyRotation());
 
-            enemy.transform.parent = _spawnArea.transform;
+                enemy.transform.parent = _spawnArea.transform;
 
-            _enemyCount++;
+                AddEnemy(enemy);
 
-            yield return wait;
-        }        
+                yield return wait;
+            }
+            yield return wait_2;
+        }
+    }
+
+    private void AddEnemy(GameObject enemy)
+    {
+        EnemyCountList.Add(enemy);
+    }
+
+    private void RemoveEnemy()
+    {
+        EnemyCountList.RemoveAll(enemy => enemy == null);
     }
 
     private Quaternion RandomEnemyRotation()
