dd9754809f8013c44197ab540434a24cac730c80
Gramer20 <alexeiduninovo@gmail.com>
2026-04-29 16:27:26 +0400

    Add collision with explosion

--- AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/DealDamageAndDestroy.cs
+++ AnnihilatorsFromAnotherWorld/Assets/GameProject/Scripts/DealDamageAndDestroy.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class DealDamageAndDestroy : MonoBehaviour
 {
-    [SerializeField] private float _bulletDamage = 10f;
+    [SerializeField] private float _damage = 10f;
+
+    [SerializeField] private float _explosionRadius = 2f;
+    [SerializeField] private float _explosionPower = 25f;
+
+    [SerializeField] private Rigidbody _rigidbody;
 
     private void OnCollisionEnter(Collision col)
     {
-        LayerMask layerMask = 1 << col.gameObject.layer;
-
         if (col.gameObject.TryGetComponent<ObjectDurability>(out var durability))
         {
-            durability.TakeDamage(_bulletDamage);
+            durability.TakeDamage(_damage);
+            _rigidbody.AddExplosionForce(_explosionPower, gameObject.transform.position, _explosionRadius);
         }
 
         Destroy(gameObject);
